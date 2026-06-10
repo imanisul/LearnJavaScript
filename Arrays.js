36ed3016027bdc75221c837e69e37b46d9231654
imanisul <ianisul768@gmail.com>
2026-06-10 23:55:44 +0530

    array

---
+++ Arrays.js
@@ -0,0 +1,8 @@
+// store multiple values 
+
+let car = ["Hundai", "Suzuki", "TATA", "Mahindra"];
+
+////////// index 0, 1, 2 , 3, 4
+
+
+console.log(car[3]);
---
+++ areaofcircle.js
@@ -0,0 +1,6 @@
+function area (radius){
+
+    return 3.14 * radius * radius
+}
+
+console.log(area(6));
