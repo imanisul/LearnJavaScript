792f93088fcbf54f63595f1db106c0860f850f39
imanisul <ianisul768@gmail.com>
2026-06-10 23:18:12 +0530

    variables, operations

---
+++ variables.js
@@ -0,0 +1,34 @@
+let name = "Anisul" // string
+
+let age = 23  // number
+
+let isstudent = true // boolean
+
+let x = null // null value
+
+let y // undefined
+
+console.log(name);
+console.log(age);
+console.log(isstudent);
+console.log(x);
+console.log(y);
+
+
+// real example 
+
+let productName = "Lenovo Slim 3";
+let price = 50000;
+let category = "Laptop"
+let isAvailable = true;
+
+
+console.log(productName);
+console.log(price);
+console.log(category);
+console.log(isAvailable);
+
+
+
+
+
