7caf0505325ee747ad27994f8fb9eca8b2dd1e43
imanisul <ianisul768@gmail.com>
2026-06-11 00:33:26 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,15 @@
+let student = {
+    name : "Anisul",
+    age : 24,
+    followers : 500,
+    email :"ianisl@gmail.com"
+}
+
+
+console.log(student.age);
+
+console.log(student.name);
+
+console.log(student.email);
+
+console.log(student.followers);
