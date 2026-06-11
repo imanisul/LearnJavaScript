d596790b72ca06e922aabdbec2293916f3090446
imanisul <ianisul768@gmail.com>
2026-06-11 21:16:08 +0530

    spread

---
+++ spread.js
@@ -0,0 +1,17 @@
+//The Spread Operator (...) is used to spread (unpack) elements of an array or properties of an object into another array, object, or function call.
+
+const names = ["Ani", "Manu", "Jef"];
+
+const nameusers = [...names]
+
+// const users = [names]
+
+// console.log(users);  
+
+
+nameusers.push("Arif")
+
+console.log(names);
+console.log(nameusers);
+
+
