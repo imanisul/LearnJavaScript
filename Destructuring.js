6bd77181061e00beb23faa591314c3b4ed80a357
imanisul <ianisul768@gmail.com>
2026-06-11 09:49:23 +0530

    distructuring

---
+++ Destructuring.js
@@ -0,0 +1,16 @@
+//Destructuring is unpacking items from a box into a separate conatiners
+
+
+let student = {
+    name : "Anisul",
+    age : 24,
+    UID : 2014
+}
+
+let {name, age, UID} = student;
+
+console.log(name);
+console.log(age);
+console.log(UID);
+
+
