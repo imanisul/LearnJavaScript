3554884d0b77675785df12b223660ac2d64b4f4f
imanisul <ianisul768@gmail.com>
2026-06-11 09:54:50 +0530

    arraydestructuring

---
+++ arrayDestructuring.js
@@ -0,0 +1,17 @@
+
+
+const fruits = ["Apple", "Mango", "Banana", "Graphes"];
+
+const [first, sec, trd, fourth] = fruits;
+
+
+console.log(fruits);
+
+
+console.log(first);
+console.log(sec);
+console.log(trd);
+console.log(fourth);
+
+
+
